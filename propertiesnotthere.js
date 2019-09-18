a8a1f42eea901e95386a42cba73b706ebe2afdeb
julio-crypt0 <hernandezjulio58@ymail.com>
2019-09-18 13:28:01 -0500

    added beginning of drill propertiesisnotthere

---
+++ propertiesnotthere.js
@@ -0,0 +1,35 @@
+'use strict';
+let obj1 = {
+  name : 'Fred',
+  jobTitle : 'Chef'
+};
+let obj2 = {
+  name : 'Chuck',
+  jobTitle : 'Prep Cook',
+  boss : 'Fred'
+};
+let obj3 = {
+  name : 'Dylan',
+  jobTitle : 'Sous Chef',
+  boss : 'Fred'
+};
+let obj4 = {
+  name : 'Randy',
+  jobTitle : 'Dishwasher',
+  boss : 'Fred'
+};
+let obj5 = {
+  name : 'Nelly',
+  jobTitle : 'General Manager',
+  boss : 'Fred'
+};
+  
+let arr = [obj1, obj2, obj3, obj4, obj5];
+
+
+arr.forEach({
+        if()
+        function(index){
+    console.log(`${index.jobTitle} ${index.name} reports to ${index.boss}.`);
+    };
+})
